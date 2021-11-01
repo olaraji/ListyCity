17bd98567ad7e1a130d04dfab66fb737189dbef0
Ola Raji <oraji@ualberta.ca>
2021-11-01 08:04:48 -0700

    GREEN: Count cities functionality passed

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -61,9 +61,7 @@ public class CustomList extends ArrayAdapter<City> {
         }
     }
     public int countCities(){
-//        return cities.size();
-
-        return 0;
+        return cities.size();
     }
 
 }
